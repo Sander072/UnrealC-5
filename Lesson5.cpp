0265e228f4144bfae711e0dfbd514d30afac8e44
Sander072 <sander072@mail.ru>
2024-05-18 14:49:31 +0300

    Добавьте файлы проекта.

---
+++ Lesson5.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+
+void Cycle(int n)
+{
+    for (int c = 0; c <= n; c += 2)
+        std::cout << c << " ";
+}
+
+void MirrorСycle(int n)
+{
+    for (int c = 1; c <= n; c += 2)
+        std::cout << c << " ";
+}
+
+void Сondition(int n, bool s)
+{
+    if (s == true)
+        Cycle(n);
+    else
+        MirrorСycle(n);
+}
+
+int main()
+{
+    int n = 20;
+    Cycle(n);
+    std::cout << "\n";
+
+    bool a = true;
+    Сondition(n, a);
+    std::cout << "\n";
+
+    bool b = false;
+    Сondition(n, b);
+}
